7b93be2e63248ce80b93cc81d3b4bcec3e02021d
BrennanZDAS <jonathon_lau@hotmail.com>
2023-01-18 15:33:08 -0500

    Update ParentingHelper.cs

--- Assets/ParentingHelper.cs
+++ Assets/ParentingHelper.cs
@@ -3,44 +3,25 @@ using UnityEngine;
 
 public class ParentingHelper : MonoBehaviour
 {
-    GameLauncher gl = null;
-
     bool transfered = false;
-
-    void Start()
-    {
-        GameLauncher gl = FindObjectOfType<GameLauncher>();
-    }
 
     void Update()
     {
-        if(!transfered)
+        if (!transfered)
         {
-            if(gl)
-            {
-                NetworkObject[] networkObjects = FindObjectsOfType<NetworkObject>();
+            NetworkObject[] networkObjects = FindObjectsOfType<NetworkObject>();
 
-                for (int i = 0; i < networkObjects.Length; i++)
+            for (int i = 0; i < networkObjects.Length; i++)
+            {
+                if (name.Contains("p#" + i + 1))
                 {
-                    if (name.Contains("p#" + i+1))
+                    if (networkObjects[i].Id.Raw == i + 3)
                     {
-                        if(networkObjects[i].Id.ToString() == (i+3).ToString())
-                        {
-                            transform.parent = networkObjects[i].transform;
-                            transfered = true;
-                        }
-                        
+                        transform.parent = networkObjects[i].transform;
+                        transfered = true;
                     }
-                }
-
-                for (int i = 1; i < gl.playerIndex + 1; i++)
-                {
 
                 }
-            }
-            else
-            {
-                gl = FindObjectOfType<GameLauncher>();
             }
         }
     }
